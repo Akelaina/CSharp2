cfc35c7de4f076128b41673286b7ae7e79c41b05
Akelaina <evgeniyarych@gmail.com>
2019-03-27 20:57:14 +0500

    Создала новое решение в Windows Forms. Домашнее задание по 1 занятию.

--- MyGame/MyGame/Form1.cs
+++ MyGame/MyGame/Form1.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Rychina_lesson1
+namespace MyGame
 {
-    class Program
+    public partial class Form1 : Form
     {
-        static void Main(string[] args)
+        public Form1()
         {
-
+            InitializeComponent();
         }
     }
 }
