3732eca9b3ed98f2efa7a72f4241df8b1fe39565
Baksieuro <Baksieuro@gmail.com>
2019-04-11 02:29:01 +0500

    Добавила новые классы Департаментов и Сотрудников

---
+++ BaseCompany/BaseCompany/Classes/Department.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseCompany
+{
+    class Department
+    {
+        /// <summary>
+        /// Принимаем значения названия департамента
+        /// </summary>
+        public string Name { get; set; }
+        
+        /// <summary>
+        /// Инициализируем департамент
+        /// </summary>
+        /// <param name="name">Название</param>
+        public Department (string name)
+        {
+            Name = name;
+        }
+        
+        /// <summary>
+        /// Метод вывода департамента на экран
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"{Name}";
+
+        }
+    }
+}
