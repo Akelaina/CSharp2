8bef4b317959a7c048ce5bc2ba7b9a5a31268a78
Baksieuro <Baksieuro@gmail.com>
2019-04-15 23:13:33 +0500

    Добавила новый класс для работы с базой данных и внесла изменения в остальные классы

--- BaseCompany/BaseCompany/Classes/Employee.cs
+++ BaseCompany/BaseCompany/Classes/Employee.cs
@@ -4,16 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BaseCompany
-{
-    class Employee
-    {
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 
-        public string Name { get; set; }
+namespace BaseCompany.Classes
+{
+    class Employee : IEquatable<Employee>, INotifyPropertyChanged
+    {
+        string employeeName;
+        public string Name
+        {
+            get { return this.employeeName; }
+            set
+            {
+                this.employeeName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.Name)));
+            }
+        }
+
         public string Surname { get; set; }
         public uint Age { get; set; }
         public decimal Salary { get; set; }
-        public string Department { get; set; }
+        public uint DepartmentID { get; set; }
 
         /// <summary>
         /// Инициализация работника
@@ -24,18 +36,70 @@ namespace BaseCompany
         /// <param name="salary">Зарплата</param>
         /// <param name="department">Отдел</param>
 
-        public Employee (string name, string surname, uint age, decimal salary, string department)
+        public Employee (string name, string surname, uint age, decimal salary, uint depid)
         {
             Name = name;
             Surname = surname;
             Age = age;
             Salary = salary;
-            Department = department;
-        }
-        public override string ToString()
-        {
-            return $"{Name},\n{Surname},\n{Age},\n{Salary},\n{Department}";
+            DepartmentID = depid;
         }
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //Сергей Камянецкий приказал убрать :)
+        /// <summary>
+        /// Возвращет имя и фамилию сотрудника
+        /// </summary>
+        /// <returns></returns>
+        //public override string ToString()
+        //{
+        //    return $"{Name} {Surname}";
+        //}
+
+        /// <summary>
+        /// Возвращает информацию о сотруднике
+        /// </summary>
+        /// <returns></returns>
+        public string GetInfo()
+        {
+            return $"\nИмя: {Name}\nФамилия: {Surname}\nВозраст: {Age}\nЗарплата: {Salary}\n" +
+                $"Отдел: {GetDepartmentName(MainWindow.database.GetDepartments() as ObservableCollection<Department>)}\n";
+        }
+
+        /// <summary>
+        /// Сравнение сотрудников
+        /// </summary>
+        /// <param name="another"></param>
+        /// <returns></returns>
+        public bool Equals(Employee another)
+        {
+            if (this.Name == another.Name && this.Surname == another.Surname && this.Age == another.Age &&
+                this.DepartmentID == another.DepartmentID && this.Salary == another.Salary)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает название отдела
+        /// </summary>
+        /// <param name="list">Список отделов</param>
+        /// <returns></returns>
+        internal string GetDepartmentName(ObservableCollection<Department> list)
+        {
+            var request = from e
+                          in list
+                          where e.DepartmentID == DepartmentID
+                          select e;
+
+            string result = (request.ElementAt(0)).Name;
+
+            return result;
+        }
     }
 }
